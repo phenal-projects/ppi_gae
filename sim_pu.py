bde1668fcc2c7dd789adab7b868decae09e1b355
Ilya Bushmakin <isnicsi@gmail.com>
2020-08-04 17:35:37 +0300

    tensor/numpy problems fixed

--- sim_pu.py
+++ sim_pu.py
@@ -23,5 +23,7 @@ def knn_prob(X, y, k):
 
 def prob_labels(y, probs):
     """Returns random labels according to probs. Leaves ones."""
+    y = torch.tensor(y)
+    probs = torch.tensor()
     thr = np.random.sample(len(probs))
     return torch.logical_or(probs > torch.tensor(thr), y).long()
