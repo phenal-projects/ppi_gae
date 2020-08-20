d36502275a16579c099a724ce98c39e0271002a2
Ilya Bushmakin <isnicsi@gmail.com>
2020-08-20 16:13:43 +0300

    GAT

--- ctd.py
+++ ctd.py
@@ -130,7 +130,7 @@ full_graph.adj_t = SparseTensor(
 mlflow.set_tracking_uri("http://localhost:12345")
 
 with mlflow.start_run():
-    model = gnn.GAE(gae.SimpleEncoder(62, args.dim, len(node_classes)))
+    model = gnn.GAE(gae.CTDEncoder(62, args.dim, len(node_classes)))
     optimizer = opt.AdamW(
         model.parameters(), args.lr, weight_decay=args.wd, amsgrad=True
     )
